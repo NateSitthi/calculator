96f5562181bb3ee019690f707dc15cbe482968ec
NateSitthi <nate.sitthi23@student.cart.org>
2024-01-25 15:49:17 -0800

    ah

--- calculator.js
+++ calculator.js
@@ -1,7 +1,18 @@
-let stat1 = " ", stat2 = " ", operator = " ";
-
+let num1 = document.getElementById(num1);
+let num2 = document.getElementById(num2);
+let ops = document.getElementById(ops);
+let num1num = parseFloat(num1.value);
+let num2num = parseFloat(num2.value);
+let answer = document.getElementById(mainAnswer);
 
 function calc() {
-    document.getElementById(num1)
-    
+switch (ops) {
+    case `+`:
+    result = `$(parseFloat(num1) + parseFloat(num2))`    
+        break;
+
+    default:
+        break;
+}
+return result;
 }
